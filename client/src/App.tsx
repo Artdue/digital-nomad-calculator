6d098ba128ed49613410b00efb21cd32e8dfac24
AleArtiu <rbirbrty0@gmail.com>
2023-08-09 17:22:57 +0300

    CompanyServices

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { Button } from 'flowbite-react';
 import './App.css';
-import { Provider } from 'react-redux';
-import Calculator from './components/MainPage/Calculator/Calculator';
-import store from './Redux/store';
+import CompanyServices from './components/MainPage/CompanyServices/CompanyServices';
 
 function App(): JSX.Element {
-  return (
-    <Provider store={store}>
-      <Button>Работает</Button>;
-      <Calculator />
-    </Provider>
-  )
+  return <CompanyServices />;
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import store from './Redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
-  </BrowserRouter>,
+  </Provider>,
 );
