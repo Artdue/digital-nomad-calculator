1f927164614f24f8d95a495caef606bca20c978e
Supermax95 <wallmax@mail.ru>
2023-08-09 13:32:55 +0700

    Multer

--- client/src/components/ProfilePage/Profile.tsx
+++ client/src/components/ProfilePage/Profile.tsx
@@ -1,5 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export default function Profile() {
-  return <div>Profile</div>;
+function Profile() {
+  const handleFileUpload = async (file, type) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      await axios.post(`http://localhost:3000/profile/${type}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(`Файл типа ${type} успешно загружен на сервер`);
+    } catch (error) {
+      console.error(`Ошибка при загрузке файла ${type}:`, error.response);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Профиль пользователя</h1>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const file = e.target.elements.passportFile.files[0];
+            handleFileUpload(file, 'passport');
+          }}
+        >
+          <label>Загрузите паспорт:</label>
+          <input type="file" name="passportFile" />
+          <button type="submit">Загрузить</button>
+        </form>
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const file = e.target.elements.balanceFile.files[0];
+            handleFileUpload(file, 'balance');
+          }}
+        >
+          <label>Загрузите банковскую выписку:</label>
+          <input type="file" name="balanceFile" />
+          <button type="submit">Загрузить</button>
+        </form>
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const file = e.target.elements.leaseFile.files[0];
+            handleFileUpload(file, 'lease');
+          }}
+        >
+          <label>Загрузите справку о работе:</label>
+          <input type="file" name="leaseFile" />
+          <button type="submit">Загрузить</button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default Profile;
--- server/router/profileRouter.js
+++ server/router/profileRouter.js
@@ -1,7 +1,92 @@
-const router = require("express").Router();
+const express = require('express');
+const multer = require('multer');
+const { User } = require('../db/models');
 
-router.get("/", (req, res) => {
-  res.json("бэк работает");
+const profileRouter = express.Router();
+
+const storagePassport = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './uploads/passport');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.originalname}`);
+  },
 });
 
-module.exports = router;
+const storageBalance = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './uploads/balance');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+const storageLease = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './uploads/lease');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+const uploadsPassport = multer({ storage: storagePassport });
+const uploadsBalance = multer({ storage: storageBalance });
+const uploadsLease = multer({ storage: storageLease });
+
+// profileRouter.post('/passport', uploadsPassport.single('file'), async (req, res) => {
+//   const { userId } = req.session;
+//   const originalname = req.file.filename;
+
+//   await User.update({ passport: `/uploads/passport/${originalname}` }, { where: { id: userId } });
+//   res.send('Паспорт успешно загружен.');
+// });
+
+// profileRouter.post('/balance', uploadsBalance.single('file'), async (req, res) => {
+//   const { userId } = req.session;
+//   const originalname = req.file.filename;
+
+//   await User.update({ balance: `/uploads/balance/${originalname}` }, { where: { id: userId } });
+//   res.send('Банковская выписка успешно загружена.');
+// });
+
+// profileRouter.post('/lease', uploadsLease.single('file'), async (req, res) => {
+//   const { userId } = req.session;
+//   const originalname = req.file.filename;
+
+//   await User.update({ lease: `/uploads/lease/${originalname}` }, { where: { id: userId } });
+//   res.send('Справка о работе успешно загружена.');
+// });
+
+// profileRouter.post('/passport', uploadsPassport.single('file'), async (req, res) => {
+//   const userId = 1; // Замените на нужный сид пользователя
+//   const originalname = req.file.filename;
+
+//   await User.update({ passport: `/uploads/passport/${originalname}` }, { where: { id: userId } });
+//   res.send('Паспорт успешно загружен.');
+// });
+profileRouter.post('/passport', uploadsPassport.single('file'), async (req, res) => {
+  const userId = 1; 
+  const originalname = req.file.filename;
+  
+    await User.update({ passport: `/uploads/passport/${originalname}` }, { where: { id: userId } });
+    res.send('Паспорт успешно загружен.');
+  });
+profileRouter.post('/balance', uploadsBalance.single('file'), async (req, res) => {
+  const userId = 1;
+  const originalname = req.file.filename;
+
+  await User.update({ balance: `/uploads/balance/${originalname}` }, { where: { id: userId } });
+  res.send('Банковская выписка успешно загружена.');
+});
+
+profileRouter.post('/lease', uploadsLease.single('file'), async (req, res) => {
+  const userId = 1; 
+  const originalname = req.file.filename;
+
+  await User.update({ lease: `/uploads/lease/${originalname}` }, { where: { id: userId } });
+  res.send('Справка о работе успешно загружена.');
+});
+
+module.exports = profileRouter;
