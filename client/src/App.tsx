d41ff9c5b9dd8bcd366bfe6c413e7af1f7d47100
AleArtiu <rbirbrty0@gmail.com>
2023-08-09 13:08:07 +0300

    Компонет Strengths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Button } from 'flowbite-react';
 import './App.css';
+import Strengths from './components/MainPage/Strengths/Strengths';
 
 function App(): JSX.Element {
-  return (  );
+  return <Strengths />;
 }
 
 export default App;
