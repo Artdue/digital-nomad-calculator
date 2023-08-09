68bb8e75c16800c46cc9ef30044a6134d9e6c3b5
AleArtiu <rbirbrty0@gmail.com>
2023-08-09 09:41:19 +0300

    info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Button } from 'flowbite-react';
 import './App.css';
+import AboutСompany from './components/AboutСompany/AboutСompany';
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
+import FeedbackForm from './components/FeedbackForm/FeedbackForm';
 
 function App(): JSX.Element {
-  return <Button>Работает</Button>;
+  return (
+    <>
+      <AboutСompany />
+      <PrivacyPolicy />
+      <FeedbackForm />
+    </>
+  );
 }
 
 export default App;
