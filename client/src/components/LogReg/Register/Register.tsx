6205491479df7572f1e84abcf5fa7bc060133538
Supermax95 <wallmax@mail.ru>
2023-08-13 21:03:19 +0700

    85%READY

--- client/src/components/LogReg/Register/Register.tsx
+++ client/src/components/LogReg/Register/Register.tsx
@@ -125,15 +125,6 @@ export default function Register() {
           </Link>
           <span className="border-b w-1/5 md:w-1/4" />
         </div>
-        {/* <div className="mt-10">
-        <button
-  type="submit"
-  className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
->
-  <a href="http://localhost:3000/google">Зарегистрироваться через gmail</a>
-</button> 
-
-        </div> */}
         <RegGoogle/>
       </form>
 
