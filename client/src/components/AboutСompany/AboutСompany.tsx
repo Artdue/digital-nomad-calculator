2559811e4db07a65358944e7eb2e56249c721a5f
AleArtiu <rbirbrty0@gmail.com>
2023-08-17 16:04:45 +0300

    BOZHE KOGDA ENTO ZAKONCHITTSYA

--- client/src/components/AboutСompany/AboutСompany.tsx
+++ client/src/components/AboutСompany/AboutСompany.tsx
@@ -32,7 +32,7 @@ export default function AboutCompany() {
   };
   return (
     <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-      <div className="absolute inset-0 -z-10 overflow-hidden">
+      {/* <div className="absolute inset-0 -z-10 overflow-hidden">
         <svg
           className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
           aria-hidden="true"
@@ -62,7 +62,7 @@ export default function AboutCompany() {
             fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
           />
         </svg>
-      </div>
+      </div> */}
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
@@ -82,6 +82,7 @@ export default function AboutCompany() {
             </div>
           </div>
         </div>
+        {/* что было */}
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
@@ -89,6 +90,14 @@ export default function AboutCompany() {
             alt=""
           />
         </div>
+        {/* что есть */}
+        {/* <div className="ml-12 -mt-12 lg:p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden md:top-0  md:overflow-visible  sm:overflow-visible">
+          <img
+            className="w-[48rem] lg:max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10"
+            src="https://uploads-ssl.webflow.com/63d8af1349fdb34d6fab174e/642161a203b6c1c3a58f80f2_THE_COST_AND_THE_PRO.png"
+            alt=""
+          />
+        </div> */}
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
