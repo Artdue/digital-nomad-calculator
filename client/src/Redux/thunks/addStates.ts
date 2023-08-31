16c6057d2b473bede24ba1c16bef2c318e4f1bd5
Supermax95 <wallmax@mail.ru>
2023-09-01 00:25:48 +0700

    fffffiiiiiixxxxxxxxiiiik

--- client/src/Redux/thunks/addStates.ts
+++ client/src/Redux/thunks/addStates.ts
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import axios  from 'axios';
+import type { Istate } from '../../Types/types';
 
-export const addState = createAsyncThunk('admin/addState', async (newState) => {
-  try {
-    const response = await axios.post('http://localhost:3000/admin', newState);
+ const addState = createAsyncThunk<Istate>('admin/addState', async (newState) => {
+    const response: AxiosResponse<Istate>  = await axios.post('http://localhost:3000/admin', newState);    
     return response.data;
-  } catch (error) {
-    throw error;
-  }
+
 });
+export default addState;
