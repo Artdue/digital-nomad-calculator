6201392a3d5178f0e450ff232b04463b4f8f478f
AleArtiu <rbirbrty0@gmail.com>
2023-09-01 15:58:54 +0300

    fix

--- client/src/Redux/thunks/editUsersList.ts
+++ client/src/Redux/thunks/editUsersList.ts
@@ -6,7 +6,7 @@ import { IstateData } from '../store.types';
 export const editUser = createAsyncThunk('admin/editState', async (stateData: IstateData) => {
   const { id, data } = stateData;
   const response: AxiosResponse<IstateData> = await axios.put(
-    http://localhost:3000/admin/users/${id},
+    `http://localhost:3000/admin/users/${id}`,
     data,
   );
   return response.data;
