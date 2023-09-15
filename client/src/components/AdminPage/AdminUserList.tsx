42e3adbf4d9410deca198b7b67fa4227362fbec0
AleArtiu <rbirbrty0@gmail.com>
2023-09-15 13:28:00 +0300

    newStateModal

--- client/src/components/AdminPage/AdminUserList.tsx
+++ client/src/components/AdminPage/AdminUserList.tsx
@@ -244,13 +244,6 @@ function AdminUserList(): React.JSX.Element {
         />
         {/* Модальное окно */}
         {showModal && (
-          // <div className="flex items-stretch overflow-auto">
-          //   <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 sm:h-[full]  ">
-          //     <div className="bg-white p-4 rounded-md w-[1000px] h-[90vh]">
-          //       <div className="px-4 sm:px-0 text-center ">
-          //         <h1 className="text-2xl font-bold leading-7 text-gray-900">Анкета</h1>
-          //       </div>
-
           <div className="flex items-stretch overflow-auto">
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 sm:h-[full]">
               <div className="bg-white p-4 rounded-md w-[1000px] h-[90vh]">
