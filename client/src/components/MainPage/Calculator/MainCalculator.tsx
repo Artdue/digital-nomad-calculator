46ebe9b8da5effbf7f683f4e09f742b0e022cc30
Artdue <arttdude@gmail.com>
2023-09-18 13:43:14 +0300

    done

--- client/src/components/MainPage/Calculator/MainCalculator.tsx
+++ client/src/components/MainPage/Calculator/MainCalculator.tsx
@@ -192,21 +192,6 @@ export default function MainCalculator(): React.JSX.Element {
             />
             <rect id="ground" x="52" y="44" fill="#002F5F" width="40" height="4" />
           </svg>
-          {/* <svg
-            version="1.1"
-            id="airplane-loader"
-            xmlns="http://www.w3.org/2000/svg"
-            width="144"
-            height="48"
-            viewBox="0 0 144 48"
-          >
-            <path
-              id="airplane-landing"
-              fill="#002F5F"
-              d="M59.124,34L56,29h-4l2.947,11H89c1.657,0,3-1.343,3-3s-1.343-3-3-3H78.998L69,18h-4l4.287,16H59.124z"
-            />
-            <rect id="ground" x="52" y="44" fill="#002F5F" width="40" height="4" />
-          </svg> */}
         </div>
       ) : (
         <div className="flex flex-row space-x-4 justify-center">
