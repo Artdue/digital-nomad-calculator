a54f7649a00bb66a61c9e7b487730681ae77ad58
Artdue <arttdude@gmail.com>
2023-11-07 16:17:02 +0300

    ohohoh

--- server/database.js
+++ server/database.js
@@ -9,7 +9,7 @@ const { Pool } = require('pg');
 // });
 
 const pool = new Pool({
-  connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
+  connectionString: `${process.env.DB_URL}?sslmode=require`,
 });
 
 pool.connect((err) => {
